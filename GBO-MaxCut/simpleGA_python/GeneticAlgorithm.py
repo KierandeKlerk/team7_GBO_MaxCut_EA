efbcce1ebe1170365a0959535a20fa06a85a113e
Sebastian <sebastianspeekenbrink@gmail.com>
2023-05-25 23:59:48 +0200

    Improve code styling

--- GBO-MaxCut/simpleGA_python/GeneticAlgorithm.py
+++ GBO-MaxCut/simpleGA_python/GeneticAlgorithm.py
@@ -12,30 +12,27 @@ from Utils import ValueToReachFoundException
 class GeneticAlgorithm:
     def __init__(self, fitness: FitnessFunction, population_size, **options):
         self.fitness = fitness
-        self.evaluation_budget = 1000000
-        self.variation_operator = Variation.uniform_crossover
+        self.evaluation_budget = options.get("evaluation_budget", 1000000)
+        self.variation_operator = self._get_variation_operator(options.get("variation", "UniformCrossover"))
         self.selection_operator = Selection.tournament_selection
         self.population_size = population_size
         self.population = []
         self.number_of_generations = 0
-        self.verbose = False
+        self.verbose = options.get("verbose", False)
         self.print_final_results = True
 
-        if "verbose" in options:
-            self.verbose = options["verbose"]
-
-        if "evaluation_budget" in options:
-            self.evaluation_budget = options["evaluation_budget"]
-
-        if "variation" in options:
-            if options["variation"] == "UniformCrossover":
-                self.variation_operator = Variation.uniform_crossover
-            elif options["variation"] == "OnePointCrossover":
-                self.variation_operator = Variation.one_point_crossover
-            elif options["variation"] == "TwoPointCrossover":
-                self.variation_operator = Variation.two_point_crossover
-            elif options["variation"] == "CustomCrossover":
-                self.variation_operator = partial(Variation.custom_crossover, self.fitness)
+    def _get_variation_operator(self, variation):
+        match variation:
+            case "UniformCrossover":
+                return Variation.uniform_crossover
+            case "OnePointCrossover":
+                return Variation.one_point_crossover
+            case "TwoPointCrossover":
+                return Variation.two_point_crossover
+            case "CustomCrossover":
+                return partial(Variation.custom_crossover, self.fitness)
+            case _:
+                raise ValueError(f"Unknown variation operator: {variation}")
 
     def initialize_population(self):
         self.population = [Individual.initialize_uniform_at_random(self.fitness.dimensionality) for i in
@@ -58,31 +55,33 @@ class GeneticAlgorithm:
 
     def print_statistics(self):
         fitness_list = [ind.fitness for ind in self.population]
-        print("Generation {}: Best_fitness: {:.1f}, Avg._fitness: {:.3f}, Nr._of_evaluations: {}".format(
-            self.number_of_generations, max(fitness_list), np.mean(fitness_list), self.fitness.number_of_evaluations))
+        print(f"Generation {self.number_of_generations}:"
+              f"Best_fitness: {max(fitness_list)},"
+              f"Avg._fitness: {np.mean(fitness_list)},"
+              f"Nr._of_evaluations: {self.fitness.number_of_evaluations}")
 
     def get_best_fitness(self):
-        return max([ind.fitness for ind in self.population])
+        return max(ind.fitness for ind in self.population)
 
     def run(self):
         try:
             self.initialize_population()
-            while (self.fitness.number_of_evaluations < self.evaluation_budget):
+            while self.fitness.number_of_evaluations < self.evaluation_budget:
                 self.number_of_generations += 1
-                if (self.verbose and self.number_of_generations % 100 == 0):
+                if self.verbose and self.number_of_generations % 100 == 0:
                     self.print_statistics()
 
                 offspring = self.make_offspring()
                 selection = self.make_selection(offspring)
                 self.population = selection
-            if (self.verbose):
+            if self.verbose:
                 self.print_statistics()
         except ValueToReachFoundException as exception:
-            if (self.print_final_results):
+            if self.print_final_results:
                 print(exception)
-                print("Best fitness: {:.1f}, Nr._of_evaluations: {}".format(exception.individual.fitness,
-                                                                            self.fitness.number_of_evaluations))
+                print(f"Best fitness: {exception.individual.fitness},"
+                      f"Nr._of_evaluations: {self.fitness.number_of_evaluations}")
             return exception.individual.fitness, self.fitness.number_of_evaluations
-        if (self.print_final_results):
+        if self.print_final_results:
             self.print_statistics()
         return self.get_best_fitness(), self.fitness.number_of_evaluations
