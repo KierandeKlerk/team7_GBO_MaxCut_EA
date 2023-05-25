5663294190259e044647ee227acd715471aed68d
Sebastian <sebastianspeekenbrink@gmail.com>
2023-05-26 00:11:04 +0200

    Individual.py improvement

--- GBO-MaxCut/simpleGA_python/Individual.py
+++ GBO-MaxCut/simpleGA_python/Individual.py
@@ -6,6 +6,7 @@ class Individual:
         self.genotype = np.array(genotype)
         self.fitness = 0
 
+    @staticmethod
     def initialize_uniform_at_random(genotype_length):
         individual = Individual()
         individual.genotype = np.random.choice((0, 1), p=(0.5, 0.5), size=genotype_length)
