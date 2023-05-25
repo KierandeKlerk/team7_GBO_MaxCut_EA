02d4314a36fc331fc080063f53e5989f4dc060ea
KierandeKlerk <kierandeklerk@gmail.com>
2023-05-25 23:44:20 +0200

    Added flush and sync commands to ensure data gets written to disk even if program does not finish, also it hopefully makes sure memory doesn't fill up as quickly

--- GBO-MaxCut/simpleGA_python/data_gathering.py
+++ GBO-MaxCut/simpleGA_python/data_gathering.py
@@ -27,7 +27,7 @@ if __name__ == "__main__":
             f.write("dimensionality, num_edges, population_size,success_rate, min, 10th_percentile,50th_percentile,90th_percentile, max\n")
             population_size = 500
             num_evaluations_list = []
-            num_runs = 10
+            num_runs = 10   
             for i, inst in enumerate(filesIn):
                 print("\nInstance {}/{} \n".format(i+1,num_files_in))
                 num_success = 0
@@ -42,14 +42,5 @@ if __name__ == "__main__":
                 print("{} evaluations (median)".format(np.median(num_evaluations_list)))
                 percentiles = np.percentile(num_evaluations_list,[10,50,90])
                 f.write("{},{},{},{},{},{},{},{},{}\n".format(fitness.dimensionality,len(fitness.edge_list),population_size,num_success/num_runs,np.min(num_evaluations_list),percentiles[0],percentiles[1],percentiles[2],np.max(num_evaluations_list)))
-            # for i in range(num_runs):
-            #     fitness = FitnessFunction.MaxCut(inst)
-            #     genetic_algorithm = GeneticAlgorithm(fitness,population_size,variation=cx,evaluation_budget=100000,verbose=False)
-            #     best_fitness, num_evaluations = genetic_algorithm.run()
-            #     if best_fitness == fitness.value_to_reach:
-            #         num_success += 1
-            #     num_evaluations_list.append(num_evaluations)
-            # print("{}/{} runs successful".format(num_success,num_runs))
-            # print("{} evaluations (median)".format(np.median(num_evaluations_list)))
-            # percentiles = np.percentile(num_evaluations_list,[10,50,90])
-            # f.write("{} {} {} {} {}\n".format(population_size,num_success/num_runs,percentiles[0],percentiles[1],percentiles[2]))
+                f.flush()
+                os.fsync(f)
