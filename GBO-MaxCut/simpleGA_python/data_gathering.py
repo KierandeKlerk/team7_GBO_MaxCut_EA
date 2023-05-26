59c0960e7584bd8aa0c2b657419663cd6f7f15b1
Sebastian <sebastianspeekenbrink@gmail.com>
2023-05-26 12:21:48 +0200

    Multiple files

--- GBO-MaxCut/simpleGA_python/data_gathering.py
+++ GBO-MaxCut/simpleGA_python/data_gathering.py
@@ -7,7 +7,7 @@ from GeneticAlgorithm import GeneticAlgorithm
 import FitnessFunction
 
 def run_genetic_algorithm(run_params):
-    i, inst, population_size, cx = run_params
+    inst, population_size, cx = run_params
     fitness = FitnessFunction.MaxCut(inst)
     genetic_algorithm = GeneticAlgorithm(fitness, population_size, variation=cx, evaluation_budget=100000, verbose=False)
     best_fitness, num_evaluations = genetic_algorithm.run()
@@ -31,21 +31,17 @@ if __name__ == "__main__":
         with open(os.path.join(dirOut,file_out),"w") as f:
             f.write("dimensionality, num_edges, population_size,success_rate, min, 10th_percentile,50th_percentile,90th_percentile, max\n")
             population_size = 500
-            num_runs = 10
             pool = mp.Pool()  # creates a pool of worker processes
 
-            for i, inst in enumerate(filesIn):
+            run_params = [(inst, population_size, cx) for inst in filesIn]
+            results = pool.map(run_genetic_algorithm, run_params)
+
+            for i, result in enumerate(results):
                 print("\nInstance {}/{} \n".format(i+1,num_files_in))
-                run_params = [(i, inst, population_size, cx) for _ in range(num_runs)]
-                results = pool.map(run_genetic_algorithm, run_params)
-                num_success = sum(success for success, _, _, _ in results)
-                num_evaluations_list = [evaluations for _, evaluations, _, _ in results]
-                print("{}/{} runs successful".format(num_success,num_runs))
-                print("{} evaluations (median)".format(np.median(num_evaluations_list)))
-                dimensionality = results[0][2]
-                num_edges = results[0][3]
-                percentiles = np.percentile(num_evaluations_list,[10,50,90])
-                f.write("{},{},{},{},{},{},{},{},{}\n".format(dimensionality,num_edges,population_size,num_success/num_runs,np.min(num_evaluations_list),percentiles[0],percentiles[1],percentiles[2],np.max(num_evaluations_list)))
+                success, num_evaluations, dimensionality, num_edges = result
+                print("{}/{} run successful".format(success, 1))
+                print("{} evaluations (total)".format(num_evaluations))
+                f.write("{},{},{},{},{},{},{},{},{}\n".format(dimensionality,num_edges,population_size,success,num_evaluations,num_evaluations,num_evaluations,num_evaluations,num_evaluations))
                 f.flush()
                 os.fsync(f.fileno())
             pool.close()  # close the pool to prevent any more tasks from being submitted
