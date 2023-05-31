a4c4fa258926c05733bd229b35ac58a4d1a36690
KierandeKlerk <kierandeklerk@gmail.com>
2023-05-31 22:44:17 +0200

    Added script that does 30 runs for each combination of graph dimensionality and population size and stores data in csv file (stops running a combination once it fails)

---
+++ GBO-MaxCut/simpleGA_python/pop_size_optimum.py
@@ -0,0 +1,57 @@
+import os
+import datetime
+import multiprocessing as mp
+import numpy as np  
+
+from GeneticAlgorithm import GeneticAlgorithm
+import FitnessFunction
+
+def run_genetic_algorithm_pop_size_optimization(run_params):
+    inst, population_size, cx, n_runs = run_params
+    success = True
+    list_num_evaluations = []
+    for _ in range(n_runs):
+        fitness = FitnessFunction.MaxCut(inst)
+        genetic_algorithm = GeneticAlgorithm(fitness, population_size, variation=cx, evaluation_budget=100000, verbose=False)
+        best_fitness, num_evaluations = genetic_algorithm.run()
+        list_num_evaluations.append(num_evaluations)
+        if best_fitness != fitness.value_to_reach: 
+            print("Population size: {} failed".format(population_size)) 
+            success = False
+            break
+
+    return success, np.median(list_num_evaluations), fitness.dimensionality, len(fitness.edge_list), population_size
+
+crossovers = ["UniformCrossover", "OnePointCrossover"]
+set = "A"
+
+dirIn = "maxcut-instances/set{}".format(set)
+dirOut = "output/set{}".format(set)
+currentDT = datetime.datetime.now()
+os.makedirs(dirOut,exist_ok=True)
+
+filesIn = [os.path.join(dirIn, file) for file in os.listdir(dirIn) if file.endswith(".txt")]
+num_files_in = len(filesIn)
+filesIn.sort()
+
+n_runs = 30
+
+for cx in crossovers:
+    file_out = "output-pop_size-{}_{}.csv".format(cx, currentDT.strftime("%d-%m-%Y_%H-%M"))
+    with open(os.path.join(dirOut,file_out),"w") as f:
+        f.write("dimensionality, num_edges, population_size, success, median_num_eval\n")
+        
+        run_params = [(inst, population_size, cx, n_runs) for inst in filesIn[0::10] for population_size in [10, 50, 100, 500, 1000, 5000, 10000]]
+        
+        pool = mp.Pool()  # creates a pool of worker processes
+        results = pool.map(run_genetic_algorithm_pop_size_optimization, run_params)
+        for i, result in enumerate(results):
+            success, median_num_evaluations, dimensionality, num_edges, population_size = result
+            print("\nDimensionality: {}, Population size: {}, Success, {} \n".format(dimensionality, population_size, success))
+            f.write("{},{},{},{},{}\n".format(dimensionality,num_edges,population_size,success,median_num_evaluations))
+            f.flush()
+            os.fsync(f.fileno())
+
+        pool.close()  # close the pool to prevent any more tasks from being submitted
+        pool.join()  # wait for all the worker processes to terminate
+
