ea67d7bcdf6cc2bfaba160a3c45b64d32adb1100
KierandeKlerk <kierandeklerk@gmail.com>
2023-06-08 10:23:15 +0200

    Added evaluation opitimization script and resulting data for all sets

--- GBO-MaxCut/simpleGA_python/evaluations_optimization.py
+++ GBO-MaxCut/simpleGA_python/evaluations_optimization.py
@@ -6,23 +6,24 @@ import numpy as np
 from GeneticAlgorithm import GeneticAlgorithm
 import FitnessFunction
 
-def run_genetic_algorithm_pop_size_optimization(run_params):
-    inst, population_size, cx, n_runs = run_params
+def run_genetic_algorithm_eval_optimization(run_params):
+    inst, evaluation_budget, cx, n_runs = run_params
+    population_size = 10000
     success = True
     list_num_evaluations = []
     for _ in range(n_runs):
         fitness = FitnessFunction.MaxCut(inst)
-        genetic_algorithm = GeneticAlgorithm(fitness, population_size, variation=cx, evaluation_budget=100000, verbose=False)
+        genetic_algorithm = GeneticAlgorithm(fitness, population_size, variation=cx, evaluation_budget=evaluation_budget, verbose=False)
         best_fitness, num_evaluations = genetic_algorithm.run()
         list_num_evaluations.append(num_evaluations)
         if best_fitness != fitness.value_to_reach: 
-            print("Population size: {} failed".format(population_size)) 
+            print("Evaluations: {} failed".format(evaluation_budget)) 
             success = False
             break
 
     return success, np.median(list_num_evaluations), fitness.dimensionality, len(fitness.edge_list), population_size
 
-def pop_size_optimization(set):
+def eval_optimization(set):
     crossovers = ["UniformCrossover", "OnePointCrossover"]
 
 
@@ -37,17 +38,17 @@ def pop_size_optimization(set):
     n_runs = 10
 
     for cx in crossovers:
-        file_out = "output-pop_size-{}_{}.csv".format(cx, currentDT.strftime("%d-%m-%Y_%H-%M"))
+        file_out = "output-num_eval-{}_{}.csv".format(cx, currentDT.strftime("%d-%m-%Y_%H-%M"))
         with open(os.path.join(dirOut,file_out),"w") as f:
             f.write("dimensionality,num_edges,population_size,success,median_num_eval\n")
             
-            run_params = [(inst, population_size, cx, n_runs) for inst in filesIn[0::10] for population_size in [50, 100, 500, 1000, 5000, 10000, 50000, 100000, 200000, 300000, 400000, 500000, 750000, 1000000]]
+            run_params = [(inst, evaluation_budget, cx, n_runs) for inst in filesIn[0::10] for evaluation_budget in [10, 100, 1000, 5000, 10000, 50000, 100000, 200000, 500000, 750000, 1000000]]
             
             pool = mp.Pool()  # creates a pool of worker processes
-            results = pool.map(run_genetic_algorithm_pop_size_optimization, run_params)
+            results = pool.map(run_genetic_algorithm_eval_optimization, run_params)
             for i, result in enumerate(results):
                 success, median_num_evaluations, dimensionality, num_edges, population_size = result
-                print("\nDimensionality: {}, Population size: {}, Success, {} \n".format(dimensionality, population_size, success))
+                print("\nDimensionality: {}, Median_num_evaluations: {}, Success, {} \n".format(dimensionality, median_num_evaluations, success))
                 f.write("{},{},{},{},{}\n".format(dimensionality,num_edges,population_size,success,median_num_evaluations))
                 f.flush()
                 os.fsync(f.fileno())
@@ -56,4 +57,4 @@ def pop_size_optimization(set):
             pool.join()  # wait for all the worker processes to terminate
 
 if __name__ == "__main__":
-    pop_size_optimization("A")
+    eval_optimization("A")
